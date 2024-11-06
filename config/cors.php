82298a492dfa963aa1a953219a47a7ae0ea15c7a
Renato Moor <renatomoor1@gmail.com>
2024-11-06 10:48:36 +0100

    chore: expose precognition headers

--- config/cors.php
+++ config/cors.php
@@ -19,13 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['precognition', 'precognition-success'],
 
     'max_age' => 0,
 
